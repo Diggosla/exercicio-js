b9e0f320da02381ab02543fed72fb7429dd39384
Diggosla <diogozanetti28@gmail.com>
2022-11-29 19:16:21 -0300

    ex-005
    ex-005 pronto

---
+++ ex005/app.js
@@ -0,0 +1,13 @@
+function CalcularMedia(){
+     
+    let n1 = document.getElementById("nota1").value
+    let n2 = document.getElementById("nota2").value
+    let n3 = document.getElementById("nota3").value
+    let n4 = document.getElementById("nota4").value
+    
+    document.write(n1*n1 , "<br>") 
+    document.write(n2*n2 ,"<br>")
+    document.write(n3*n3 ,"<br>")
+    document.write(n4*n4 ,"<br>")
+    document.write(n5*n5 ,"<br>")
+}
