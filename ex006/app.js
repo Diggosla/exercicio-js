364008d511327c7906b4dd7d74540ac75ba4c6ef
Diggosla <diogozanetti28@gmail.com>
2022-11-29 21:08:28 -0300

    ex-006
    ex-006 incompleto

---
+++ ex006/app.js
@@ -0,0 +1,17 @@
+function Mostrar() {
+    let nome = document.getElementById("nome").value
+    let idade = document.getElementById("idade").value
+    let altura = document.getElementById("altura").value
+    document.getElementById("n").innerHTML = "seu nome é:" + nome
+    document.getElementById("i").innerHTML = "sua altura é:" + altura
+    document.getElementById("a").innerHTML = "sua idade é:" + idade
+}
+
+
+
+function apagaForm() {
+    document.getElementById("nome").reset();
+    document.getElementById("altura").reset();
+    document.getElementById("idade").reset();
+}
+
