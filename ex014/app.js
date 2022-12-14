e5297abdfe8eacfefd515107c2b46887e0dbc456
Diggosla <diogozanetti28@gmail.com>
2022-12-13 22:18:50 -0300

    atividade-js
    atividade java script

--- ex014/app.js
+++ ex014/app.js
@@ -1,11 +1,9 @@
-function MaiorDeIdade() {
+function Idade() {
     let idade = document.getElementById("idade").value
 
     if(idade >=18 ){
         document.getElementById("resposta").innerHTML = "vc é maior de idade"
     } else {
         document.getElementById("resposta").innerHTML = "vc é menor de idade"
-
-
     }
 }
